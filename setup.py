7255a3213418fe4bb3365bd60537f7e88af0c4cd
Samantha Dixon <sam.dixon@berkeley.edu>
2017-07-23 15:56:59 -0700

    Add bare-bones setup.py and build file structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='pensieve',
+      version='0.0.1',
+      description=u"A Python package to extract character mems from a corpus of text",
+      author=u"CDIPS-AI 2017",
+      author_email='sam.dixon@berkeley.edu',
+      url='https://github.com/CDIPS-AI-2017/pensieve',
+      license='Apache 2.0',
+      )
